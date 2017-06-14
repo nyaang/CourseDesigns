a622b54d92d3b8ac25be80d966059d989a15eded
YANGzzp <akazaakarinnn@gmail.com>
2017-06-14 12:03:32 +0800

    update
    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
-
 using namespace std;
-//以十个人七项任务为例
 
 typedef struct CTNODE {
 	double cost; //存放个人经费消耗
@@ -12,6 +10,7 @@ typedef struct CTNODE {
 	double allcost; //记录总的费用，遍历的时候会用上
 	CTNODE* child; //子结点
 };
+
 //创建并初始化树，给所有结点的person_num和task_num赋值
 void createchildnode(CTNODE& parnode, int m, int n) {
 	if (parnode.person_num < n) {
@@ -29,11 +28,11 @@ void createchildnode(CTNODE& parnode, int m, int n) {
 }
 
 //遍历树
-void inittree(CTNODE& parnode, double cost[100][100], int n, int m,CTNODE* leafnode,int& leafnum) {	//需要提供一个根节点的引用，和cost矩阵
+void inittree(CTNODE& parnode, double cost[100][100], int n, int m, CTNODE* leafnode, int& leafnum) {	//需要提供一个根节点的引用，和cost矩阵
 	//非叶子结点的情况
 	bool travelvalue = 1;	//辅助变量，用于判断是否需要遍历该结点
 	//如果子结点的task_num已经出现在母结点的travelist中，则不遍历该结点
-	if (parnode.person_num <n) {
+	if (parnode.person_num < n) {
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (parnode.child[i].task_num = parnode.travellist[j]) {
@@ -45,7 +44,7 @@ void inittree(CTNODE& parnode, double cost[100][100], int n, int m,CTNODE* leafn
 				parnode.child[i].cost = cost[parnode.child[i].person_num][parnode.child[i].task_num];
 				parnode.child[i].allcost = parnode.allcost + parnode.child[i].cost;
 				parnode.child[i].travellist[parnode.child[i].person_num] = parnode.child[i].task_num;
-				inittree(parnode.child[i], cost, n, m,leafnode,leafnum);
+				inittree(parnode.child[i], cost, n, m, leafnode, leafnum);
 			}
 			travelvalue = 1;
 		}
@@ -60,9 +59,8 @@ void inittree(CTNODE& parnode, double cost[100][100], int n, int m,CTNODE* leafn
 }
 
 
-
-void sortcost(CTNODE* leafnode,int leafnum,int n) {
-	CTNODE minleaf=leafnode[1];	//辅助变量，存放最优解
+void sortcost(CTNODE* leafnode, int leafnum, int n) {
+	CTNODE minleaf = leafnode[1];	//辅助变量，存放最优解
 	for (int i = 2; i <= leafnum; i++) {
 		if (minleaf.allcost < leafnode[i].allcost) {
 			minleaf = leafnode[i];
@@ -84,7 +82,6 @@ int jiecheng(int x) {
 	return mul;
 }
 
-
 int main()
 {
 	////n个人，m个任务
@@ -100,19 +97,19 @@ int main()
 			cin >> cost[i][j];
 	}
 	rootnode.cost = 0; rootnode.person_num = 0; rootnode.task_num = 0;	//根结点的数据成员无实际意义，全设为0
-	rootnode.allcost = 0; 
-	rootnode.travellist = new int[n] ;
+	rootnode.allcost = 0;
+	rootnode.travellist = new int[n];
 	for (int i = 1; i <= n; i++) {
 		rootnode.travellist[i] = -1;
 	}
 	rootnode.child = new CTNODE[m + 1];
-	createchildnode(rootnode, m,n);
+	createchildnode(rootnode, m, n);
 
 
 	int leafsize = jiecheng(n) / jiecheng(n - m);
-	CTNODE* leafnode = new CTNODE[leafsize+1];	//辅助CTNODE数组，存放所有的叶子结点
+	CTNODE* leafnode = new CTNODE[leafsize + 1];	//辅助CTNODE数组，存放所有的叶子结点
 	int leafnum = 0;	//辅助变量，记录CTNODE中有多少个叶子结点
-	inittree(rootnode, cost, n, m,leafnode,leafnum);
+	inittree(rootnode, cost, n, m, leafnode, leafnum);
 	sortcost(leafnode, leafnum, n);
 	system("pause");
 	return 0;
